f57408fbe6aadd4f469f2179b9d61146cf95592d
SergeHall <sergioartg@gmail.com>
2022-03-23 16:25:03 -0700

    6 kyu Does my number look big in this?

--- 05_kyu_Calculating with Functions.js
+++ 05_kyu_Calculating with Functions.js
@@ -1,4 +1,5 @@
-// This time we want to write calculations using functions and get the results. Let's have a look at some examples:
+// This time we want to write calculations using functions and get the results. Let's have a
+// look at some examples:
 //
 // seven(times(five())); // must return 35
 // four(plus(nine())); // must return 13
@@ -7,11 +8,12 @@
 // Requirements:
 //
 // There must be a function for each number from 0 ("zero") to 9 ("nine")
-// There must be a function for each of the following mathematical operations: plus, minus, times, dividedBy
+// There must be a function for each of the following mathematical operations: plus, minus,
+// times, dividedBy
 // Each calculation consist of exactly one operation and two numbers
-// The most outer function represents the left operand, the most inner function represents the right operand
-// Division should be integer division. For example, this should return 2, not 2.666666...:
-// eight(dividedBy(three()));
+// The most outer function represents the left operand, the most inner function represents the
+// right operand Division should be integer division. For example, this should return 2,
+// not 2.666666...: eight(dividedBy(three()));
 
 
 function zero(func) {
