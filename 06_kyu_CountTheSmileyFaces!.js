ce876f7535af56f56a794bd8d2c6c256dbe5db91
SergeHall <sergioartg@gmail.com>
2022-03-25 22:07:43 -0700

    6 kyu Count the smiley faces!

---
+++ 06_kyu_CountTheSmileyFaces!.js
@@ -0,0 +1,39 @@
+// Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+//
+// Rules for a smiling face:
+//
+// Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+// A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+// Every smiling face must have a smiling mouth that should be marked with either ) or D
+// No additional characters are allowed except for those mentioned.
+//
+// Valid smiley face examples: :) :D ;-D :~)
+// Invalid smiley faces: ;( :> :} :]
+//
+// Example
+//
+// countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+// countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+// countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+
+
+function countSmileys(arr) {
+  let sum = 0;
+  for (let i of arr) {
+    console.log(i.slice(1, 2));
+    if (i.length === 2) {
+      if ((i.slice(0, 1) === ":" || i.slice(0, 1) === ";")
+        && (i.slice(-1) === "D" || i.slice(-1) === ")")) {
+        sum += 1;
+      }
+    }
+    if (i.length === 3) {
+      if ((i.slice(0, 1) === ":" || i.slice(0, 1) === ";")
+        && (i.slice(1, 2) === "-" || i.slice(1, 2) === "~")
+        && (i.slice(-1) === "D" || i.slice(-1) === ")")) {
+        sum += 1;
+      }
+    }
+  }
+  return sum
+}
